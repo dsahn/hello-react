f8229e5aade9b0da1f7fb1baa5adea0d7e238095
Dong-Su An <ads50112@gmail.com>
2018-06-23 01:01:16 +0900

    style 객체 사용 연습

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
+    const style = {
+      backgroundColor: 'black',
+      padding: '16px',
+      color: 'white',
+      fontSize: '33px'
+    };
     return (
-      <div>
+      <div style={style}>
         <h1>안녕하세요 리액트</h1>
       </div>
     );
